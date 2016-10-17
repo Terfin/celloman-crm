31be2beba6e344bc688dd3e6537c2f224fdca3fd
Nadav Har Tzvi <freddyvod@gmail.com>
2016-10-17 13:04:51 +0300

    UI application added and done a basic login flow

--- app.py
+++ app.py
@@ -12,9 +12,14 @@ def login():
     if req.status_code == 200:
         token = req.json()['token']
         req = r.get('http://127.0.0.1:8000/api/users/current/', headers={'Authorization': 'Token {}'.format(token)})
-        user = req.json()
-        user['token'] = token
-        return jsonify(user)
+        print(req.headers)
+        if req.status_code == 200:
+            user = req.json()
+            user['token'] = token
+            return jsonify(user)
+        else:
+            print(req.json())
+            return '', 500
     else:
         return '', 401
 
